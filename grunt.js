772260b557275e6936e5154b70789001232da4ac
Stephen Crosby <stevecrozz@gmail.com>
2012-10-21 13:43:26 -0700

    Incorporate grunt and various other reorganizations
     * Move the CodeMirror submodule into vendor/
     * Move tcp-client.js to vendor/
     * Move all the local code to lib/
     * Create the grunt file
     * Remove xregexp.js (unnecessary library)
     * Rewrite the ruby-debug-client execution stack as an array of
       callbacks rather than an array of instructions

---
+++ grunt.js
@@ -0,0 +1,53 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    meta: {
+      version: '0.1.0',
+      banner: '/*! uBugger - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* http://PROJECT_WEBSITE/\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+        'Stephen Crosby; No License Yet */'
+    },
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        jQuery: false,
+        CodeMirror: false,
+        TcpClient: false,
+        CommandPrompt: false,
+        RubyDebugClient: false,
+        chrome: false
+      }
+    },
+    uglify: {}
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint qunit');
+
+};
