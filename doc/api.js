ab7e982c5b1da3b888869da5460f44aa4dbe5fa1
Stephen Crosby <stevecrozz@gmail.com>
2013-01-21 23:02:43 -0800

    begin adding YUIdoc

---
+++ doc/api.js
@@ -0,0 +1,34 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CommandPrompt",
+        "DebuggerUi",
+        "DebuggerUi.View",
+        "DebuggerUi.View.BreakpointView",
+        "DebuggerUi.View.CodeView",
+        "RubyDebugClient"
+    ],
+    "modules": [
+        "CommandPrompt",
+        "DebuggerUi",
+        "RubyDebugClient"
+    ],
+    "allModules": [
+        {
+            "displayName": "CommandPrompt",
+            "name": "CommandPrompt",
+            "description": "Exports the CommandPrompt class"
+        },
+        {
+            "displayName": "DebuggerUi",
+            "name": "DebuggerUi",
+            "description": "Exports the DebuggerUi class"
+        },
+        {
+            "displayName": "RubyDebugClient",
+            "name": "RubyDebugClient",
+            "description": "Provides the RubyDebugClient class"
+        }
+    ]
+} };
+});
