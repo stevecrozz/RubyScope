d014e950b98383364c6e250f6729fbe593f7c8a8
Stephen Crosby <stevecrozz@gmail.com>
2013-01-24 18:56:39 -0800

    commit initial design courtesy of Jason Melgoza

---
+++ doc/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CommandPrompt",
+        "DebuggerUi",
+        "DebuggerUi.View",
+        "DebuggerUi.View.BreakpointView",
+        "DebuggerUi.View.CodeView",
+        "DebuggerUi.View.ConsoleView",
+        "DebuggerUi.View.ControlView",
+        "RubyDebugClient"
+    ],
+    "modules": [
+        "CommandPrompt",
+        "DebuggerUi",
+        "RubyDebugClient"
+    ],
+    "allModules": [
+        {
+            "displayName": "CommandPrompt",
+            "name": "CommandPrompt",
+            "description": "Exports the CommandPrompt class"
+        },
+        {
+            "displayName": "DebuggerUi",
+            "name": "DebuggerUi",
+            "description": "Exports the DebuggerUi class"
+        },
+        {
+            "displayName": "RubyDebugClient",
+            "name": "RubyDebugClient",
+            "description": "Provides the RubyDebugClient class"
+        }
+    ]
+} };
+});
